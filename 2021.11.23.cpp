428d139ac40ac1b30abe63e9e60232aabc600c45
jason-1220-ui <83058251+jason-1220-ui@users.noreply.github.com>
2021-11-24 22:59:03 +0800

    Add files via upload

--- 2021.11.23.cpp
+++ 2021.11.23.cpp
@@ -2,30 +2,146 @@
 #include <string>
 using namespace std;
 
-//反转字符串 II[https://leetcode-cn.com/problems/reverse-string-ii/]
+//字符串中的第一个唯一字符（返回索引）[https://leetcode-cn.com/problems/first-unique-character-in-a-string/]
+//反向查找法
 class Solution {
 public:
-	void reverse(string &s, int start, int end) {
-		int left = start;
-		int right = end - 1;
-		while (left < right) {
-			char tmp = s[left];
-			s[left] = s[right];
-			s[right] = tmp;
-			left++;
-			right--;
+	int firstUniqChar(string s) {
+		for (int i = 0; i < s.size(); ++i) {
+			int index = s.find(s[i]);
+			int rindex = s.rfind(s[i]);
+			if (index == rindex)
+				return i;
 		}
-	}
-	string reverseStr(string s, int k) {
-		for (int i = 0; i < s.size(); i += 2 * k) {
-			if (i + k < s.size())
-				reverse(s, i, i + k);
-			else
-				reverse(s, i, s.size());
-		}
-		return s;
+		return -1;
 	}
 };
+//哈希表
+/*
+class Solution {
+public:
+	int firstUniqChar(string s) {
+		int hash[256] = { 0 };
+		for (int i = 0; i < s.size(); ++i)
+			hash[s[i]]++;
+		for (int i = 0; i < s.size(); ++i) {
+			if (hash[s[i]] == 1)
+				return i;
+		}
+		return -1;
+	}
+};*/
+//暴力求解法
+/*
+class Solution {
+public:
+	int firstUniqChar(string s) {
+		int j;
+		for (int i = 0; i < s.size(); ++i) {
+			for (j = 0; j < s.size(); ++j) {
+				if (i == j)
+					continue;
+				if (s[i] == s[j])
+					break;
+			}
+			if (j >= s.size())
+				return i;
+		}
+		return -1;
+	}
+};*/
+
+//字符串相乘[https://leetcode-cn.com/problems/multiply-strings/]
+/*
+class Solution {
+public:
+	void MulItem(string &num, int s, string &tmp) {
+		int sign = 0;
+		for (int i = 0; i < num.size(); ++i) {
+			int mul = (num[i] - '0') * s + sign;
+			if (mul >= 10) {
+				sign = mul / 10;
+				mul %= 10;
+			}
+			else {
+				sign = 0;
+			}
+			tmp.push_back(mul + '0');
+		}
+		if (sign > 0)
+			tmp.push_back(sign + '0');
+	}
+	int AddItem(int a, int b, int &sign) {
+		int sum = a + b + sign;
+		if (sum >= 10) {
+			sum -= 10;
+			sign = 1;
+		}
+		else
+			sign = 0;
+		return sum;
+	}
+	void AddMove(string &num, string &tmp, int k) {
+		int i = k;
+		int j = 0;
+		int sign = 0;
+		while (i < num.size() && j < tmp.size()) {
+			num[i] = AddItem(num[i] - '0', tmp[j] - '0', sign) + '0';
+			i++;
+			j++;
+		}
+		while (i < num.size()) {
+			num[i] = AddItem(num[i] - '0', 0, sign) + '0';
+			i++;
+		}
+		while (j < tmp.size()) {
+			int sum = AddItem(0, tmp[j] - '0', sign);
+			num.push_back(sum + '0');
+			j++;
+		}
+		if (sign > 0)
+			num.push_back(sign + '0');
+	}
+	string multiply(string num1, string num2) {
+		if (num1 == "0" || num2 == "0")
+			return string("0");
+		reverse(num1.begin(), num1.end());
+		reverse(num2.begin(), num2.end());
+		string tmp, result;
+		for (int i = 0; i < num2.size(); ++i) {
+			MulItem(num1, num2[i] - '0', tmp);
+			AddMove(result, tmp, i);
+			tmp.clear();
+		}
+		reverse(result.begin(), result.end());
+		return result;
+	}
+};*/
+
+//反转字符串 II[https://leetcode-cn.com/problems/reverse-string-ii/]
+//class Solution {
+//public:
+//	void reverse(string &s, int start, int end) {
+//		int left = start;
+//		int right = end - 1;
+//		while (left < right) {
+//			char tmp = s[left];
+//			s[left] = s[right];
+//			s[right] = tmp;
+//			left++;
+//			right--;
+//		}
+//	}
+//	string reverseStr(string s, int k) {
+//		for (int i = 0; i < s.size(); i += 2 * k) {
+//			if (i + k < s.size())
+//				reverse(s, i, i + k);
+//			else
+//				reverse(s, i, s.size());
+//		}
+//		return s;
+//	}
+//};
 
 //反转字符串中的单词 III[https://leetcode-cn.com/problems/reverse-words-in-a-string-iii/]
 /*
